f140f8abfadabf2770f4390c16ca12a01a7910d3
unknown <md.r.a.n.786@gmail.com>
2024-01-27 15:41:52 +0530

    fixed footer link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ export const Footer = () => {
     return (
         <footer id="footer" className="bg-black bg-opacity-70 text-white p-4 text-center h-60 mt-10">
       <div className="flex justify-center items-center mt-10">
-        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="mr-4">
+        <a href="https://www.instagram.com/rashid_._mohammad?igsh=MTE5NG00czF5czBxaQ==" target="_blank" rel="noopener noreferrer" className="mr-4">
           <FontAwesomeIcon icon={faInstagram} className="text-5xl hover:text-gray-500 m-5" />
         </a>
         <a href="https://www.linkedin.com/in/rashid-aziz-68a208200/" target="_blank" rel="noopener noreferrer" className="mr-4">
